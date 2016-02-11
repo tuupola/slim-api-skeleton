5dc03e16e7a0685253e10643e57f5af220d8b8a8
Mika Tuupola <tuupola@appelsiini.net>
2016-02-11 08:40:52 +0700

    Mention Marcus as original author

--- lib/Utils/Base62.php
+++ lib/Utils/Base62.php
@@ -1,6 +1,18 @@
 <?php
 
-/* https://gist.github.com/Synchro/1139429 */
+/*
+ * This file is part of the Slim API skeleton package
+ *
+ * Copyright (c) 2011-2016 Marcus Bointon, Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://gist.github.com/Synchro/1139429
+ *   https://github.com/tuupola/slim-api-skeleton
+ *
+ */
 
 namespace Utils;
 
