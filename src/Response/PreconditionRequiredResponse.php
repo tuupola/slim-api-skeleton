137591ccb561ab4835d59f4cdf5700d35bd12612
Mika Tuupola <tuupola@appelsiini.net>
2017-06-17 14:21:39 +0700

    Update headers

--- src/Response/PreconditionRequiredResponse.php
+++ src/Response/PreconditionRequiredResponse.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * This file is part of the Slim API skeleton package
+ *
+ * Copyright (c) 2016-2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-api-skeleton
+ *
+ */
+
 //namespace Tuupola\Http\Response;
 namespace Response;
 
--- src/Slim/Handlers/ApiError.php
+++ src/Slim/Handlers/ApiError.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * This file is part of the Slim API skeleton package.
+ *
+ * This handler is called in case of otherwise uncaught exceptions.
+ * However due to way Slim works CORS headers are lost. It would be better
+ * to handle exceptions gracefully by returning one of the predefined
+ * error responses instead.
+ *
+ * Copyright (c) 2016-2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-api-skeleton
+ *
+ */
+
 namespace Slim\Handlers;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
