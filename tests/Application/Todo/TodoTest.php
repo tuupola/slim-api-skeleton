9fdf9dc3602e8bd7a36314a898526cc72669be3b
Mika Tuupola <tuupola@appelsiini.net>
2019-01-27 11:20:32 +0200

    Be more specific in Todo unit tests

--- tests/Application/Todo/TodoTest.php
+++ tests/Application/Todo/TodoTest.php
@@ -13,7 +13,22 @@ class TodoTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testGenerateEtag()
+    public function testShouldConstruct()
+    {
+        $todo = new Todo(new TodoUid, "Not sure?", 99, true);
+        $this->assertEquals("Not sure?", $todo->title());
+        $this->assertEquals(99, $todo->order());
+        $this->assertTrue($todo->isCompleted());
+    }
+
+    public function testShouldSetDefaultValues()
+    {
+        $todo = new Todo(new TodoUid, "Not sure?");
+        $this->assertEquals(0, $todo->order());
+        $this->assertFalse($todo->isCompleted());
+    }
+
+    public function testShouldGenerateEtag()
     {
         $todo = new Todo(new TodoUid, "Not sure?");
         $this->assertEquals(32, strlen($todo->etag()));
